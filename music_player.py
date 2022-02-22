f74a4177b6c710d3be4c806f02caba60d885d502
ariel-yap <ariel.yap96@gmail.com>
2022-02-22 15:43:35 +1300

    add folder and file dialog functionality

--- music_player.py
+++ music_player.py
@@ -1,24 +1,45 @@
+"""Quick script to hold MusicPlayer class and the contruction of this GUI to display to a user
+"""
 import sys
-import random
-import json
-import string
-import uuid
-import bson.json_util
-import datetime
 import os
-from PySide2 import QtCore, QtWidgets, QtGui, QtWebSockets, QtNetwork, QtMultimedia
-# from QtMultimedia import QMediaMetaData
+from PySide2 import QtCore, QtWidgets, QtGui, QtMultimedia
+from black import main
+
+
+class QVLine(QtWidgets.QFrame):
+    def __init__(self):
+        super(QVLine, self).__init__()
+        self.setFrameShape(QtWidgets.QFrame.VLine)
+        self.setFrameShadow(QtWidgets.QFrame.Sunken)
 
 
 class Player(QtWidgets.QMainWindow):
+    """MusicPlayer Class that is a GUI that allow a user to play and listen to music.
+
+    Args:
+        QtWidgets (QMainWindow): parent QMainWindow if child component
+
+    Features:
+    - Opens a file
+    """
+
     def __init__(self):
         super().__init__()
+        self.urls = [
+            "Bach-Orchestral-Suite-no.-3-in-D-major-BWV-1068-Download-free-sheet-music.mp3",
+            "Kevin_MacLeod_-_Canon_in_D_Major.mp3",
+            "Toccata-and-Fugue-in-D-minor-BWV-565.mp3",
+        ]
+        self.folder = "/home/ariel/work/music-player/"
+        self.opened_songs = [self.folder + song for song in self.urls]
+
+        self.current_file = self.folder + self.urls[0]
 
         self.create_layout()
         self.connect()
 
     def create_layout(self):
-        """ Creates all layouts and widgets """
+        """Creates all layouts and widgets"""
 
         style = self.style()
 
@@ -57,28 +78,38 @@ class Player(QtWidgets.QMainWindow):
         # Create interactable buttons
         button_layout = QtWidgets.QHBoxLayout()
         self.play_button = QtWidgets.QPushButton()
-        icon = QtGui.QIcon.fromTheme("media-playback-start.png",
-                                     style.standardIcon(QtWidgets.QStyle.SP_MediaPlay))
+        icon = QtGui.QIcon.fromTheme(
+            "media-playback-start-symbolic.svg",
+            style.standardIcon(QtWidgets.QStyle.SP_MediaPlay),
+        )
         self.play_button.setIcon(icon)
 
         self.previous_button = QtWidgets.QPushButton()
-        icon = QtGui.QIcon.fromTheme("media-skip-backward-symbolic.svg",
-                                     style.standardIcon(QtWidgets.QStyle.SP_MediaPlay))
+        icon = QtGui.QIcon.fromTheme(
+            "media-skip-backward-symbolic.svg",
+            style.standardIcon(QtWidgets.QStyle.SP_MediaPlay),
+        )
         self.previous_button.setIcon(icon)
 
         self.next_button = QtWidgets.QPushButton()
-        icon = QtGui.QIcon.fromTheme("media-skip-forward-symbolic.svg",
-                                     style.standardIcon(QtWidgets.QStyle.SP_MediaPlay))
+        icon = QtGui.QIcon.fromTheme(
+            "media-skip-forward-symbolic.svg",
+            style.standardIcon(QtWidgets.QStyle.SP_MediaPlay),
+        )
         self.next_button.setIcon(icon)
 
         self.shuffle_button = QtWidgets.QPushButton()
-        icon = QtGui.QIcon.fromTheme("media-playlist-shuffle-symbolic.svg",
-                                     style.standardIcon(QtWidgets.QStyle.SP_MediaPlay))
+        icon = QtGui.QIcon.fromTheme(
+            "media-playlist-shuffle-symbolic.svg",
+            style.standardIcon(QtWidgets.QStyle.SP_MediaPlay),
+        )
         self.shuffle_button.setIcon(icon)
 
         self.repeat_button = QtWidgets.QPushButton()
-        icon = QtGui.QIcon.fromTheme("media-playlist-repeat-symbolic.svg",
-                                     style.standardIcon(QtWidgets.QStyle.SP_MediaPlay))
+        icon = QtGui.QIcon.fromTheme(
+            "media-playlist-repeat-symbolic.svg",
+            style.standardIcon(QtWidgets.QStyle.SP_MediaPlay),
+        )
         self.repeat_button.setIcon(icon)
 
         button_layout.addWidget(self.shuffle_button)
@@ -87,17 +118,6 @@ class Player(QtWidgets.QMainWindow):
         button_layout.addWidget(self.next_button)
         button_layout.addWidget(self.repeat_button)
         main_layout.addLayout(button_layout, 3, 0, 1, 5)
-
-        # volume_label = QtWidgets.QLabel()
-        # icon = QtGui.QIcon.fromTheme("audio-volume-medium-symbolic.svg",
-        #                              style.standardIcon(QtWidgets.QStyle.SP_MediaPlay))
-        # volume_label.setPixmap(icon.pixmap(QtCore.QSize(20, 20)))
-        # main_layout.addWidget(volume_label, 3, 3, 1, 1)
-        # self.volume_slider = QtWidgets.QSlider()
-        # self.volume_slider.setOrientation(QtGui.Qt.Horizontal)
-        # self.volume_slider.setMinimum(0)
-        # self.volume_slider.setMaximum(10)
-        # main_layout.addWidget(self.volume_slider, 3, 4, 1, 1)
 
         # create slider
         self.player_slider = QtWidgets.QSlider()
@@ -110,6 +130,11 @@ class Player(QtWidgets.QMainWindow):
         main_layout.addWidget(self.current_duration, 5, 0, 1, 1)
         main_layout.addWidget(self.total_duration, 5, 4, 1, 1)
 
+        # create separator and playlist
+        main_layout.addWidget(QVLine(), 0, 5, 6, 1)
+        self.playlist = QtWidgets.QListWidget()
+        self.playlist.addItems(self.urls)
+        main_layout.addWidget(self.playlist, 0, 6, 6, 1)
         # create the media player
         self.audio_player = QtMultimedia.QMediaPlayer()
         self.audio_player.setVolume(10)
@@ -124,47 +149,83 @@ class Player(QtWidgets.QMainWindow):
         self.audio_player.durationChanged.connect(self.setTotalDuration)
         self.player_slider.valueChanged.connect(self.setCurrentDuration)
         self.player_slider.sliderMoved.connect(self.audio_player.setPosition)
-        self.audio_player.positionChanged.connect(
-            self.player_slider.setValue)
+        self.audio_player.positionChanged.connect(self.player_slider.setValue)
         self.audio_player.mediaStatusChanged.connect(self.statusChanged)
+        self.audio_player.stateChanged.connect(self.stateChanged)
+
+        # self.playlist.itemDoubleClicked.connect(self.select_show)
 
     def open_new_song(self):
-        file_path = "/home/ariel/Work/HiringMaterials/bensound-dubstep.mp3"
-        url = QtCore.QUrl.fromLocalFile(file_path)
+        url = QtCore.QUrl.fromLocalFile(self.current_file)
         self.audio_player.setMedia(QtMultimedia.QMediaContent(url))
-        self.song_file.setText(file_path)
+        self.song_file.setText(self.current_file)
 
-    @ QtCore.Slot()
+    def reload_song_list(self, new_list):
+        self.playlist.addItems(new_list)
+
+    @QtCore.Slot()
     def statusChanged(self, status):
-        print(status)
+        # print(status)
+        pass
 
-    @ QtCore.Slot()
+    @QtCore.Slot()
+    def stateChanged(self, state):
+        style = self.style()
+        if state == QtMultimedia.QMediaPlayer.State.PlayingState:
+            icon = QtGui.QIcon.fromTheme(
+                "media-playback-pause-symbolic.svg",
+                style.standardIcon(QtWidgets.QStyle.SP_MediaPlay),
+            )
+        else:
+            icon = QtGui.QIcon.fromTheme(
+                "media-playback-start-symbolic.svg",
+                style.standardIcon(QtWidgets.QStyle.SP_MediaPlay),
+            )
+        self.play_button.setIcon(icon)
+
+        print(state)
+
+    @QtCore.Slot()
     def play_pause(self):
-        print(self.audio_player.state())
         if self.audio_player.state() == QtMultimedia.QMediaPlayer.State.PausedState:
             self.audio_player.play()
         elif self.audio_player.state() == QtMultimedia.QMediaPlayer.State.PlayingState:
             self.audio_player.pause()
-        else:
-            self.audio_player.pause()
+        elif (
+            self.audio_player.state() == QtMultimedia.QMediaPlayer.State.StoppedState
+        ) and (
+            self.audio_player.mediaStatus()
+            == QtMultimedia.QMediaPlayer.MediaStatus.LoadedMedia
+        ):
+            self.audio_player.play()
 
-    @ QtCore.Slot()
+    @QtCore.Slot()
     def open_file(self):
         selection = QtWidgets.QFileDialog.getOpenFileName(
-            self, "Select Audio File", None, "Audio Files (*.wav *.bwv)")
+            self,
+            "Select Audio File",
+            None,
+            "Audio Files (*.mp3 *.wav *.ogg *.wma *.flac)",
+        )
         if not selection:
             return
         filename = selection[0]
+        self.opened_songs.append(filename)
+        self.reload_song_list([os.path.splitext(os.path.split(filename)[1])[0]])
 
-    @ QtCore.Slot()
+    @QtCore.Slot()
     def open_folder(self):
         selected_folder = QtWidgets.QFileDialog.getExistingDirectory(
-            self, "Select the audio folder", None, QtWidgets.QFileDialog.ShowDirsOnly)
+            self, "Select a folder", None, QtWidgets.QFileDialog.ShowDirsOnly
+        )
         if not selected_folder:
             return
         files = os.listdir(selected_folder)
+        trimmed_files = [os.path.splitext(f)[0] for f in files]
+        self.opened_songs.append(files)
+        self.reload_song_list(trimmed_files)
 
-    @ QtCore.Slot()
+    @QtCore.Slot()
     def metaDataChanged(self):
         available_meta_data = self.audio_player.availableMetaData()
 
@@ -182,26 +243,18 @@ class Player(QtWidgets.QMainWindow):
         if "AudioCodec" in available_meta_data:
             print(self.audio_player.metaData("AudioCodec"))
 
-    @ QtCore.Slot()
+    @QtCore.Slot()
     def setTotalDuration(self):
-        # print(self.audio_player.duration())
         minutes = (self.audio_player.duration() // 1000) // 60
-        # print(minutes)
         seconds = (self.audio_player.duration() // 1000) % 60
-        # print(seconds)
-        self.total_duration.setText(f'{minutes:02}:{seconds:02}')
+        self.total_duration.setText(f"{minutes:02}:{seconds:02}")
         self.player_slider.setMaximum(self.audio_player.duration())
-        # self.total_duration = self.audio_player.duration
 
-    @ QtCore.Slot()
+    @QtCore.Slot()
     def setCurrentDuration(self, duration):
-        # print(self.audio_player.duration())
         minutes = (duration // 1000) // 60
-        # print(minutes)
         seconds = (duration // 1000) % 60
-        # print(seconds)
-        self.current_duration.setText(f'{minutes:02}:{seconds:02}')
-        # self.total_duration = self.audio_player.duration
+        self.current_duration.setText(f"{minutes:02}:{seconds:02}")
 
 
 if __name__ == "__main__":
@@ -212,7 +265,6 @@ if __name__ == "__main__":
         """
         QWidget
 {
-    background-color: #381422;
     font-family: Roboto-Regular;
     color: #ffffff;
 }
